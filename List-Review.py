4f039b217dc6dcdae09f827539697fe1e7954546
Kevin <kevindsouza476@gmail.com>
2024-02-07 15:29:12 -0500

    First real coding day

---
+++ List-Review.py
@@ -0,0 +1,24 @@
+# import pandas as pd
+# import numpy as np 
+
+#initial list reviews
+names = ["Tom","Mary","John","Alex","Peter"]
+
+# print(names[2:5]) 
+# print(names[2:3])
+# print(names[::2]) # mean from beginning to end, incremet by 2. so we go TOM, + , JOHN, + , Peter DONE
+# print(names[1::3]) # start 1, incre 3 until end
+# print(names[::-1]) #Back to front. hint: 0-1 = -1
+# print(names[-2::-2]) # Start at -2, then decrement -2 until end.
+# # print(names[::]) # front to back
+# for name in names: print(name)
+# for name in names[::-1]: print(name) #reverse
+# for i in range(len(names)-1,-1,-1): print(names[i]) #another way to reverse
+
+
+#dictionary
+
+# d = {'key1' : 'v1', 'key2' : "v2", 'key3' : 'v3'}
+# for key in d: print(key, d[key])
+
+
